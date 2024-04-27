7b637ae1cf7c2112293ad84e6fadb8b1b16fabe5
ortemy <ortemy@gmail.com>
2024-04-26 23:40:59 -0400

    removing certs folder

--- kafka-generator/app/src/main/java/kafka/generator/properties/AppProperties.java
+++ kafka-generator/app/src/main/java/kafka/generator/properties/AppProperties.java
@@ -13,10 +13,10 @@ public class AppProperties {
         java.util.Properties properties = new java.util.Properties();
         properties.setProperty("bootstrap.servers", "kafka-2f088c05-artem-ea11.h.aivencloud.com:23351");
         properties.setProperty("security.protocol", "SSL");
-        properties.setProperty("ssl.truststore.location", "/Users/ortemy/Dev/kafka-flink-example/kafka-generator/certs/client.truststore.jks");
+        properties.setProperty("ssl.truststore.location", "/Users/ortemy/Dev/certs/client.truststore.jks");
         properties.setProperty("ssl.truststore.password", TRUSTSTORE_PASSWORD);
         properties.setProperty("ssl.keystore.type", "PKCS12");
-        properties.setProperty("ssl.keystore.location", "/Users/ortemy/Dev/kafka-flink-example/kafka-generator/certs/client.keystore.p12");
+        properties.setProperty("ssl.keystore.location", "/Users/ortemy/Dev/certs/client.keystore.p12");
         properties.setProperty("ssl.keystore.password", KEYSTORE_PASSWORD);
         properties.setProperty("ssl.key.password", KEY_PASSWORD);
         properties.setProperty("key.serializer", StringSerializer.class.getName());
